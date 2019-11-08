289ec4af011b12e74562e46ae52ace0faa0a5e8e
qiubee <qiubees@hotmail.com>
2019-11-08 13:49:35 +0100

    update multiply-characters-in-string.js

--- modules/multiply-characters-in-string.js
+++ modules/multiply-characters-in-string.js
@@ -1,9 +1,11 @@
-function multiplyCharactersOfString(string, multiplyBy, start = 0, end = string.length) {
-    let newString = "";
-        for (start; start < end; start++) {
-                newString = newString + string[start].repeat(multiplyBy);
-            }
-    return newString;
-}
+/*jshint esversion: 6 */
 
-module.exports = multiplyCharactersOfString;
+// Function that multiplies a character in string
+exports.multiplyCharactersInString = function(string, multiplyBy, start = 0, end = string.length){
+    let newString = "";
+    // Loops over string and multiplies character
+    for (start; start < end; start++){
+    newString = newString + string[start].repeat(multiplyBy);
+    }
+    return newString;
+};
