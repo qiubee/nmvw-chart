3ef1700fd5ad0a57fa3f85976c15054c332ca9d7
qiubee <qiubees@hotmail.com>
2019-11-11 15:33:57 +0100

    add function which multiplies character in string

--- modules/multiply-characters-in-string.js
+++ modules/multiply-characters-in-string.js
@@ -1,11 +1,22 @@
 /*jshint esversion: 6 */
 
 // Function that multiplies a character in string
-exports.multiplyCharactersInString = function(string, multiplyBy, start = 0, end = string.length){
-    let newString = "";
-    // Loops over string and multiplies character
-    for (start; start < end; start++){
-    newString = newString + string[start].repeat(multiplyBy);
+exports.multiplyCharactersInString = function (string, multiplyBy, start = 0, end = string.length) {
+    if (start < end && multiplyBy >= 2) {
+        let newString = "";
+        // Loops over string and multiplies character
+        for (start; start < end; start++) {
+            newString = newString + string[start].repeat(multiplyBy);
+        }
+        if (end !== string.length) {
+            newString = newString + string.slice(end, string.length);
+        }
+        return newString;
     }
-    return newString;
+    if (start === end) {
+        console.info("start (", start, ") and end (", end, ") can't be the same number");
+    }
+    if (multiplyBy <= 1) {
+        console.info("multiplier has to be higher than 2");
+    }
 };
