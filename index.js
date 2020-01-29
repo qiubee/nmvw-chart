edc3f7b93d47a8bde4e8cb88301aa1aff1801f73
qiubee <qiubees@hotmail.com>
2020-01-29 15:07:03 +0100

    update static folder

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.use(express.static("website"));
+app.use(express.static("app"));
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
